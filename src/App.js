736af1b5b3d5dfa12da898e2d752b5551f68908d
amypo-reactapp <reactapp@amypo.in>
2023-11-27 08:48:46 +0000

    User

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+// Todolist component
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  
+  const handleAddTodo = (text) => {
+    const newTodo = { id: Date.now(), text };
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <TodoForm onAddTodo={handleAddTodo} />
+      <ul>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} todo={todo} onDeleteTodo={handleDeleteTodo} />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+// TodoForm component
+const TodoForm = ({ onAddTodo }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== '') {
+      onAddTodo(inputValue);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Add a new todo..."
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+TodoForm.propTypes = {
+  onAddTodo: PropTypes.func.isRequired,
+};
+
+// TodoItem component
+const TodoItem = ({ todo, onDeleteTodo }) => {
+  const handleDelete = () => {
+    onDeleteTodo(todo.id);
+  };
+
+  return (
+    <li>
+      {todo.text}
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+  onDeleteTodo: PropTypes.func.isRequired,
+};
+
+export default TodoList;
